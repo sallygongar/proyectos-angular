3991adbeae5cda8086f58c6a5a694a81140cee86
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2024-11-13 14:08:48 -0600

    Funciones principales del ciclo de vida

--- app-libros/src/app/app.component.ts
+++ app-libros/src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PeliculasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 }) 
-export class AppComponent implements OnInit, DoCheck {
+export class AppComponent implements OnInit, DoCheck, OnDestroy {
   title = 'app-libros';
   public autor: string;
   public titulo: string;
   public anio: number;
   public mensaje: string;
+  public mostrarPeliculas: boolean;
 
   constructor(){
     /* El constructor es donde se carga información, a las propiedades de la clase, 
@@ -22,22 +24,29 @@ export class AppComponent implements OnInit, DoCheck {
     this.titulo = "Pedro Parámo";
     this.anio = 2019;
     this.mensaje = "";
-  
-    console.log("CONSTRUCTOR LANZADO!!!")
+    this.mostrarPeliculas = true;
+    console.log("CONSTRUCTOR APP LANZADO!!!")
   }
 
   ngOnInit(): void {
     /*Aqui se puede cargar logica*/
-    console.log("COMPONENTE LANZADO")
+    console.log("COMPONENTE APP LANZADO")
   }
 
   ngDoCheck(): void {
      /* Se ejecuta cada vez que se produce un cambio en el componente */
-     console.log("DO CHECK LANZADO")
+     console.log("DO CHECK APP LANZADO")
   }
  
   addCart(): void {
     this.mensaje = "Libro agregado a carrito"
   }
 
+  ngOnDestroy(): void {
+    console.log("EL COMPONENTE APP SE VA A ELIMINAR")
+  }
+
+  ocultarPeliculas(): void{
+    this.mostrarPeliculas = false
+  }
 }
--- app-libros/src/app/components/peliculas/peliculas.component.ts
+++ app-libros/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-peliculas',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './peliculas.component.html',
   styleUrl: './peliculas.component.css'
 })
-export class PeliculasComponent {
+export class PeliculasComponent implements OnInit, DoCheck, OnDestroy{
+
+  constructor(){
+    console.log("CONSTRUCTOR PELICULA LANZADO!!!")
+  }
+
+  ngOnInit(): void {
+    console.log("COMPONENTE PELICULA LANZADO");
+  }
+
+  ngDoCheck(): void {
+    console.log("DO CHECK DE PELICULA LANZADO")
+  }
+
+  ngOnDestroy(): void {
+    /* Este componente se ejecuta antes que se quite del DOM */
+    console.log("EL COMPONENTE PELICULA SE VA A ELIMINAR")
+  }
 
 }
