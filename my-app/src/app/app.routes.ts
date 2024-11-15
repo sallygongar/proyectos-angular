322557d179404d448686f529ef6cb939f1fc9e33
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2024-11-14 20:16:00 -0600

    Creación de Rutas y page404

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'blog', title: 'Blog', component: BlogComponent },
+    { path: 'productos', title: 'Landing Productos', component: ProductoComponent },
+    { path: '**', title: 'Página no encontrada', component: Pagina404Component }
+];
