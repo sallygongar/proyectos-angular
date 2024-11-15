97d4fe6554c7f39f5ffaa56d4881129d68f623e1
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2024-11-14 20:45:42 -0600

    Creación de redirect y linkActive

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { Pagina404Component } from './components/pagina404/pagina404.component';
 export const routes: Routes = [
     { path: 'blog', title: 'Blog', component: BlogComponent },
     { path: 'productos', title: 'Landing Productos', component: ProductoComponent },
+    { path: '', redirectTo: '/blog',  pathMatch: 'full' },
     { path: '**', title: 'Página no encontrada', component: Pagina404Component }
 ];
